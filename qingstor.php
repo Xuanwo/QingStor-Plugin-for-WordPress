1d05ec833c985bff02752cd650d6d5389e2790fc
yungkcx <yungkcx@gmail.com>
2017-02-02 21:09:36 +0800

    multipart upload

--- qingstor.php
+++ qingstor.php
@@ -3,16 +3,17 @@
 Plugin Name: QingStor
 Plugin URI:  https://github.com/yungkcx/QingStor-Plugin-for-WordPress
 Description: QingStor 青云对象存储服务 WordPress 插件。
+Text Domain: qingstor
 Version:     0.1
 Author:      yungkcx
 Author URI:  http://yungkcx.github.io
 */
 
 require_once 'vendor/autoload.php';
-require_once 'qingstor-functions.php';
-require_once 'qingstor-upload.php';
-require_once 'qingstor-menu.php';
-require_once 'qingstor-backup.php';
+require_once 'php/qingstor-functions.php';
+require_once 'php/qingstor-upload.php';
+require_once 'php/qingstor-menu.php';
+require_once 'php/qingstor-backup.php';
 
 register_activation_hook(__FILE__, 'qingstor_activation');
 register_deactivation_hook(__FILE__, 'qingstor_deactivation');
